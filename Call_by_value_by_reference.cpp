349a322f4759897d39c456f5c5c26e960b7a5247
nefcodes <168460285+nefcodes@users.noreply.github.com>
2024-06-16 20:40:14 +0530

    Call_by_value_by_reference.cpp

---
+++ Call_by_value_by_reference.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void valuechange1(int x);
+void valuechange2(int& x);// Corrected function declaration
+
+int main() {
+    int num{};
+    
+    cout << "This is the original value: " << num << endl;
+    
+    valuechange1(num);
+     cout << "This is the value of num after call by value: " << num << endl;
+     
+     valuechange2(num);
+     cout << "This is the value of num after call by reference: " << num << endl;
+    
+    
+    return 0;   
+}
+
+void valuechange1(int x) {
+    x = 5;
+    cout << "This is not set to our desired value just displaying the result: " << x << endl;
+}
+
+void valuechange2(int& x) {
+    x = 5;
+    cout << "This actually changes the value of a variable: " << x << endl;
+}
