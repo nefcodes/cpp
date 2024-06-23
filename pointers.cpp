a0aafaeada5ddd8ff0d2670ae0abc89e9e8954fa
nefcodes <168460285+nefcodes@users.noreply.github.com>
2024-06-23 16:59:00 +0530

    pointers.cpp

---
+++ pointers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+  int x = 10;
+  
+  int *ptr = &x; // initialize a pointer with the address of x
+  
+  *ptr = 11; // assign 11 at the address of the pointer.
+  
+  cout << x << "\n \n";
+  
+  
+    void *ptrz = nullptr; // init a poitner to 0
+    cout << ptrz;
+    
+    
+}
