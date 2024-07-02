f290099e15a5948a6941481fcb82ff950f81fb55
nefcodes <168460285+nefcodes@users.noreply.github.com>
2024-07-02 23:58:05 +0530

    Update New[]&Delete[]Array.cpp

--- New[]&Delete[]Array.cpp
+++ New[]&Delete[]Array.cpp
@@ -12,13 +12,23 @@ using namespace std;
 
 void new_operator_array() {
 	int* p = new int[5]; //create an array of int typen the heap for 5 items and store
-						//	the address in p pointer
+	//	the address in p pointer
 
-	//Instead of initializing array with for loop you could also have written:
-	//	int* p = new int[5]{1, 2, 3, 4, 5};
+	char* meow = new char[4]; // create char array on heap to store 3 digit string, last digit for null character
+	// we did nothing,  and deleted pointer, but the value remains at the address.
+	delete[] meow; 
 	
-	//set and display all 
-	for (int i =0; i < 5; ++i) {
+
+
+
+
+//Instead of initializing array with for loop you could also have written:
+//	int* p = new int[5]{1, 2, 3, 4, 5};
+
+
+
+//set and display all 
+	for (int i = 0; i < 5; ++i) {
 		p[i] = i;
 		cout << p[i] << ", ";
 	}
