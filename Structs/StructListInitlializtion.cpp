08207375eeaace8742c94bd19c280753dd7d7253
Tanishk Shrivastava <nefcodes@gmail.com>
2024-08-02 19:31:14 +0530

    Update StructListInitlializtion.cpp

--- Structs/StructListInitlializtion.cpp
+++ Structs/StructListInitlializtion.cpp
@@ -16,6 +16,8 @@ int main() {
 	//List initialization, each item corresponds to struct data type from top to bottom
 	player cassie = { "Cassie", 25, true };
 	
+	//player cassie = { "Cassie", , true }; you could skip positions 
+	
 	cout << "Name: " << cassie.name << endl;
 	cout << "Age: " << cassie.age << endl;
 	cout << "Activity status: " << cassie.active << endl;
