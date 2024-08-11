f2169e854783f5f1e619edcc7459f67a0b62e70e
nefcodes <168460285+nefcodes@users.noreply.github.com>
2024-08-11 15:37:16 +0530

    StringFunction(incomplete).cpp

---
+++ Strings/StringFunction(incomplete).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstring> // for basic functions like: strlen, strcat, strcpy, strcat, strncpy, strcmp, strstr
+
+using namespace std;
+
+int main() {
+
+	//strlen
+	int n = strlen("wow"); // stores length in variable n (3) , can also pass in array instead of double quotes.
+	cout << n << endl;
+/*  
+    strcat ==> appends to the end of destination.
+	char destination[50] = "Hello";
+	char source[50] = "World";
+	strcat(destination, source);
+	cout << destination << endl;
+
+*/
+	
+	//strcpy
+	return 0;
+}
