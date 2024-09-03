73d689bb200fcdf33d913464c029c6a3f533795c
nefcodes <168460285+nefcodes@users.noreply.github.com>
2024-09-03 19:19:37 +0530

    Create AbsoluteBasicsOfpointer.cpp

---
+++ Pointers/AbsoluteBasicsOfpointer.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int x = 5;
+
+	cout << "Address in hexadecimal is: " <<  &x << endl;
+	cout << "size of x is: " << sizeof(x) << " bytes" << endl;
+
+	return 0;
+ 
+}
