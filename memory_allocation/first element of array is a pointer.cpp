b62d56e8775ce1a93648022ecc6589e5f08be89e
nefcodes <168460285+nefcodes@users.noreply.github.com>
2024-11-10 20:03:33 +0530

    first element of array is a pointer.cpp

--- memory_allocation/first element of array is a pointer.cpp
+++ memory_allocation/first element of array is a pointer.cpp
@@ -29,6 +29,13 @@ int main() {
 	cout << *(pointer + 2) << endl;
 	cout << *(pointer + 3) << endl;
 
+	//or
+	cout << "\n \n \n index method: " << endl;
+	cout << array[0] << endl;
+	cout << array[1] << endl;
+	cout << array[2] << endl;
+	cout << array[3] << endl;
+
 	return 0;
 
 
